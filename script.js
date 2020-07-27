a6ff4c7d771df5a5335d3cf37ecb0f61e0b61091
Frida <velia.arizpe@gmail.com>
2020-07-26 22:04:18 -0500

    testing arrays

--- script.js
+++ script.js
@@ -22,11 +22,41 @@ var wrong5 = document.getElementById("wrong5");
 var nameel = document.getElementById("options");
 var score = document.getElementById("score");
 var seescores = document.getElementById("seescores");
-var test = document.createElement("p");
-elementsArea = document.querySelectorAll("p1")
+
+var elementsArea = document.querySelector("#answers-space");
+var questionsArea = document.querySelector("#questions-space");
 var Read;
 var count = 80
 var time = document.getElementById("time");
+var number = 0
+
+
+//buttons
+
+//questions layout
+
+var questions = [
+    {
+        question: "Question 1",
+        answers: [ 
+            {
+            text: "A",
+            isCorrect: false
+            }
+        ]
+    }
+]
+
+//question loop
+var questionElement = questions [0];
+questionsArea.textContent = questionElement.question
+
+var abutton = document.createElement("button");
+varanswerElement = questionElement.answers[0].text;
+abutton.textContent = varanswerElement;
+elementsArea.appendChild(abutton);
+
+
 
 
 
@@ -57,75 +87,75 @@ function timesout(){
     }, 2000);
 }
 
-//button functions
-function closestart() {
-    startScreen.style.display = "none";
-    one.style.display ="block";
-    Timeper();
-}
-function nextclick1() {
-    one.style.display = "none";
-    two.style.display = "block";
+// //button functions
+// function closestart() {
+//     startScreen.style.display = "none";
+//     one.style.display ="block";
+//     Timeper();
+// }
+// function nextclick1() {
+//     one.style.display = "none";
+//     two.style.display = "block";
      
-}
-function nextclick2() {
-    two.style.display = "none";
-    three.style.display = "block";
+// }
+// function nextclick2() {
+//     two.style.display = "none";
+//     three.style.display = "block";
     
-}
-function nextclick3() {
-    three.style.display = "none";
-    four.style.display = "block";
+// }
+// function nextclick3() {
+//     three.style.display = "none";
+//     four.style.display = "block";
     
-}
-function nextclick4() {
-    four.style.display = "none";
-    five.style.display = "block";
+// }
+// function nextclick4() {
+//     four.style.display = "none";
+//     five.style.display = "block";
     
-}
-function nextclick5() {
-    five.style.display = "none";
-    done.style.display = "block";
-    Timestop()
-}
+// }
+// function nextclick5() {
+//     five.style.display = "none";
+//     done.style.display = "block";
+//     Timestop()
+// }
 
-//On Click
-start.addEventListener("click", closestart)
-right1.addEventListener("click", nextclick1)
-wrong1.addEventListener("click", nextclick1)
-right2.addEventListener("click", nextclick2)
-wrong2.addEventListener("click", nextclick2)
-right3.addEventListener("click", nextclick3)
-wrong3.addEventListener("click", nextclick3)
-right4.addEventListener("click", nextclick4)
-wrong4.addEventListener("click", nextclick4)
-right5.addEventListener("click", nextclick5)
-wrong5.addEventListener("click", nextclick5)
+// //On Click
+// start.addEventListener("click", closestart)
+// right1.addEventListener("click", nextclick1)
+// wrong1.addEventListener("click", nextclick1)
+// right2.addEventListener("click", nextclick2)
+// wrong2.addEventListener("click", nextclick2)
+// right3.addEventListener("click", nextclick3)
+// wrong3.addEventListener("click", nextclick3)
+// right4.addEventListener("click", nextclick4)
+// wrong4.addEventListener("click", nextclick4)
+// right5.addEventListener("click", nextclick5)
+// wrong5.addEventListener("click", nextclick5)
 
-// look at this for breveity:
-// listEl.addEventListener("click", function(event) {
+// // look at this for breveity:
+// // listEl.addEventListener("click", function(event) {
+// //     event.preventDefault();
+// //     if(event.target.matches("button")) {
+// //       var item = document.createElement("div");
+// //       item.textContent = groceries[event.target.parentElement.id];
+// //       shoppingCartEl.append(item);
+// //     }
+// //   });
+// function displayMessage(type, message) {
+//     score.textContent = message;
+//     score.setAttribute("class", type);
+// }
+
+// //local storage
+// nameel.addEventListener("click", function(event){
 //     event.preventDefault();
-//     if(event.target.matches("button")) {
-//       var item = document.createElement("div");
-//       item.textContent = groceries[event.target.parentElement.id];
-//       shoppingCartEl.append(item);
-//     }
-//   });
-function displayMessage(type, message) {
-    score.textContent = message;
-    score.setAttribute("class", type);
-}
+//     var nameels = document.getElementById("nameInput").value;
+//     if (nameels === "") {
+//         displayMessage("error", "Initials cannot be blank");
+//         }
+//     else 
+//         localStorage.setItem("Initials", nameels);
+//         done.style.display = "none";
+//         seescores.style.display = "block";
 
-//local storage
-nameel.addEventListener("click", function(event){
-    event.preventDefault();
-    var nameels = document.getElementById("nameInput").value;
-    if (nameels === "") {
-        displayMessage("error", "Initials cannot be blank");
-        }
-    else 
-        localStorage.setItem("Initials", nameels);
-        done.style.display = "none";
-        seescores.style.display = "block";
-
-});
+// });
